1a2a0f39f952c13b601371c7a6b5a696d48b8563
Ryan Chen <rc6542@princeton.edu>
2025-03-27 20:30:36 -0400

    updates profile widget wpm immediately after clicking return to menu after finishing a race

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -104,7 +104,7 @@ export const AuthProvider = ({ children }) => {
       error, 
       login, 
       logout,
-      fetchUserProfile
+      fetchUserProfile,
     }}>
       {children}
     </AuthContext.Provider>
