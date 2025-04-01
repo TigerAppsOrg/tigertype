39917ad49e3c05ca92ef5cf9b1097e04abe5de89
Ammaar Alam <ammaar@princeton.edu>
2025-04-01 19:31:17 -0400

    Simplify Vite configuration and use stable version

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,10 +27,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    sourcemap: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html')
-    }
+    sourcemap: true
   },
   test: {
     globals: true,
