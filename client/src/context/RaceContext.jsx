84e8d771652e09b51d704d9a9cb931dc0c7d0c98
Ryan Chen <rc6542@princeton.edu>
2025-04-03 15:08:47 -0400

    removed git conflicts with william by removing changes in typing, added william's button in navbar

--- client/src/context/RaceContext.jsx
+++ client/src/context/RaceContext.jsx
@@ -213,7 +213,7 @@ export const RaceProvider = ({ children }) => {
             completion_time: elapsedSeconds
           }] : prev.results
         }));
-         
+        
         // Send completion to server only for multiplayer races
         if (socket && connected && raceState.type !== 'practice') {
           socket.emit('race:result', {
