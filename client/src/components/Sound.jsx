9ade0f42fb099d2718b15888e8f273ec72e35cab
William Guan <wg6872@princeton.edu>
2025-04-10 14:27:55 -0400

    pull from master

---
+++ client/src/components/Sound.jsx
@@ -0,0 +1,15 @@
+import typingSound from '../assets/typing.mp3';
+
+const keySound = new Audio(typingSound);
+
+function playKeySound() {
+    const shouldPlaySound = localStorage.getItem('typingSound') === 'true';
+    if (shouldPlaySound) {
+
+        keySound.currentTime = 0;
+        keySound.volume = 0.2;
+        keySound.play();
+    }
+}
+
+export default playKeySound;
