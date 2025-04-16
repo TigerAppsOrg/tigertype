8c07fc0ff4290640526891d39d0e2ec43c63d382
William Guan <wg6872@princeton.edu>
2025-04-16 12:42:00 -0400

    working open/close logic for different options

--- client/src/pages/CustomLobby.jsx
+++ client/src/pages/CustomLobby.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRace } from '../context/RaceContext';
 import './CustomLobby.css';
 import Modal from '../components/Modal';
 
 function CustomLobby() {
+    const [isSelecting, setIsSelecting] = useState(true);
     const [isJoining, setIsJoining] = useState(false);
     const [isLobbyAdmin, setIsLobbyAdmin] = useState(false);
 
-    const handleJoin = () => {
+    const handleJoinEnter = () => {
         // Needs socket join emission here
-        // Needs to hide lobby select screen
+        setIsSelecting(false);
         setIsJoining(false);
+    };
+
+    const handleJoinClose = () => {
+        setIsJoining(false);
+    };
+
+    const handleCreate = () => {
+        // Needs socket join emission here
+        setIsSelecting(false);
+        setIsLobbyAdmin(true);
     };
 
     return(
@@ -18,20 +29,24 @@ function CustomLobby() {
             <Modal
                 isOpen={isJoining}
                 title="Enter Private Lobby Code"
-                message={(<div id="lobby-code-input-container">
-                            <input id="lobby-code-input" autoFocus></input>
-                            </div>)}
-                buttonText="Join lobby"
-                onClose={handleJoin}
-                onEnter={handleJoin}
+                children={(
+                    <>
+                    <div id="lobby-code-input-container">
+                        <input id="lobby-code-input" autoFocus></input>
+                    </div>
+                    </>
+                    )}
+                showCloseButton={true}
+                onClose={handleJoinClose}
+                onEnter={handleJoinEnter}
             />
 
-            <div className="lobby-select-container">
+            {isSelecting && <div className="lobby-select-container">
                 <div className="lobby-select-header">
                     <h1>Custom Lobby Options</h1>
                 </div>
                 <div>
-                    <div className="lobby-select" onClick={() => setIsLobbyAdmin(true)}>
+                    <div className="lobby-select" onClick={handleCreate}>
                         <h3>Create Private Lobby</h3>
                         <p>Invite and compete against friends</p>
                     </div>
@@ -41,6 +56,10 @@ function CustomLobby() {
                         <p>Hop into a friend's existing lobby via Lobby Code</p>
                     </div>
                 </div>
+            </div>}
+
+            <div>
+                
             </div>
         </div>
     );
