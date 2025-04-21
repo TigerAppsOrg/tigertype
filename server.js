bc8a1fccd341c89f1d9e0b16603135747b508996
Ammaar Alam <ammaar@princeton.edu>
2025-04-21 18:00:34 -0400

    removing http only requirement bc redundant

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ const sessionMiddleware = session({
   saveUninitialized: false,
   cookie: {
     secure: process.env.NODE_ENV === 'production',
-    httpOnly: true,
+    httpOnly: false,
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
     sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
     // explicitly set domain for production to avoid ambiguity with custom domains
