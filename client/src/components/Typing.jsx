01ae2bbf9591326af84325ec17862fe8b78bf084
Ammaar Alam <ammaar@princeton.edu>
2025-05-08 23:29:43 -0400

    adding caps lock warning

--- client/src/components/Typing.jsx
+++ client/src/components/Typing.jsx
@@ -904,6 +904,18 @@ function Typing({
   return (
     <>
       <div className="stats-container">
+        {/* Caps Lock Warning */}
+        {capsLockEnabled && (
+          <div className="caps-lock-warning">
+            <span className="caps-lock-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+              </svg>
+            </span>
+            Caps Lock
+          </div>
+        )}
         {getStatsContent()}
       </div>
       
@@ -946,19 +958,6 @@ function Typing({
       {showErrorMessage && (
         <div className="error-message">
           Fix your mistake to continue
-        </div>
-      )}
-
-      {/* Caps Lock Warning */}
-      {capsLockEnabled && (
-        <div className="caps-lock-warning">
-          <span className="caps-lock-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
-              <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
-            </svg>
-          </span>
-          Caps Lock
         </div>
       )}
 
